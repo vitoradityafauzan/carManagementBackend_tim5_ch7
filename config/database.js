17feb7eab2502dc60828089201b9626b892a0180
vitoradityafauzan <vitoraditya.f@gmail.com>
2022-06-03 20:23:33 +0700

    V1

---
+++ config/database.js
@@ -0,0 +1,31 @@
+/** Destruct environment variable to get database configuration */
+const {
+  DB_USERNAME = '',
+  DB_PASSWORD = '',
+  DB_HOST = '',
+  DB_NAME = '',
+} = process.env
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}`,
+    host: DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}`,
+    host: DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}`,
+    host: DB_HOST,
+    dialect: 'postgres',
+  },
+}
---
+++ config/routes.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const controllers = require('../app/controllers')
+
+const apiRouter = express.Router()
+
+// Cars
+apiRouter.get(
+  process.env.GETCARS,
+  controllers.api.v1.authController.authorize,
+  controllers.api.v1.carController.findcar,
+)
+
+/* AUTHENTICATION & AUTHORIZATION */
+
+// login users
+apiRouter.post(process.env.LOGIN, controllers.api.v1.authController.login)
+
+// register user
+apiRouter.post(process.env.REGISTER, controllers.api.v1.authController.register)
+
+// logout user
+//apiRouter.post('/api/v1/logout', controllers.api.v1.authController.logoutLocal)
+
+//Checking sessionStorage
+//apiRouter.get('/api/v1/sesi', controllers.api.v1.authController.getSesi)
+
+// check current user
+apiRouter.get(
+  process.env.AUTHCHECK,
+  controllers.api.v1.authController.authorize,
+  controllers.api.v1.authController.whoAmI,
+)
+
+// google login/register
+
+apiRouter.post(
+  process.env.GOOGLELOG,
+  controllers.api.v1.authController.handleGoogleLoginOrRegister,
+)
+
+/*
+// open Api
+apiRouter.get("/api/v1/docs/swagger.json", (req, res) => {
+  res.status(200).json(swaggerDocument);
+});
+apiRouter.use(
+  "/api/v1/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
+
+*/
+apiRouter.use(controllers.api.main.onLost)
+apiRouter.use(controllers.api.main.onError)
+
+module.exports = apiRouter
